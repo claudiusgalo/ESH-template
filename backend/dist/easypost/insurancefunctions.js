45fd290231beebf9bb02c36ac1f8245cfebff71e
claudiusgalo <galoclaudius@icloud.com>
2023-08-22 01:31:43 -0500

    Quicksave Added Multiple Entities & Associated Resolvers

--- backend/dist/easypost/insurancefunctions.js
+++
@@ -1,9 +0,0 @@
-const EasyPostClient = require('@easypost/api');
-const client = new EasyPostClient(process.env.EASYPOST_API_KEY);
-(async () => {
-    const insurances = await client.Insurance.all({
-        page_size: 5,
-    });
-    console.log(insurances);
-})();
-//# sourceMappingURL=insurancefunctions.js.map
